d609c98706b95e116ab8b341f35b4005e9296e12
renars.vilnis <renars.vilnis@warp.lv>
2015-02-06 17:53:20 +0200

    Work on getting playlist songs using async library

--- routes/index.js
+++ routes/index.js
@@ -1,45 +1,73 @@
 var express = require('express');
 var router = express.Router();
+var async = require('async');
 
 var spotify = require('../lib/spotify.js');
 
 
-getMe = function(callback) {
-  spotify.api
-    .getMe()
-    .then(function(data) {
-      return callback(null, data);
-    }, function(err) {
-      return callback(err);
-    });
-};
-
-/* GET home page. */
 router.get('/', function(req, res, next) {
 
-  getMe(function(err, data) {
-    if(err)
-      return next(err);
+  var userdata, playlists;
 
-    var userId = data.id;
-
-    spotify.api
-      .getUserPlaylists(userId, {
-        limit: 50,
-        offset: 0
-      })
-      .then(function(data) {
-        console.log('Retrieved playlists', data);
-
-        res.render('index', {
-          title: 'Hello',
-          playlists: data.items
+  async.series([
+    function getUserData(callback) {
+      spotify.api
+        .getMe()
+        .then(function(data) {
+          userdata = data;
+          callback(null);
+        }, function(err) {
+          callback(err);
         });
-      },function(err) {
-        return next(err);
+    },
+
+    function getUserPlaylists(callback) {
+      // TO-DO if user has more playlsits the api allows in single request
+      spotify.api
+        .getUserPlaylists(userdata.id, {
+          limit: 50,
+          offset: 0
+        })
+        .then(function(data) {
+          playlists = data.items;
+          callback(null, data);
+        },function(err) {
+          callback(err);
+        });
+    },
+
+    // set songs for each playlist
+    function getAllPlaylistSongs(callback) {
+      async.each(playlists, function getPlaylistSongs(playlist, callback2) {
+
+        console.log('Fething songs for', playlist.id);
+
+        // TO-DO if user has more songs then allowed in single request
+        spotify.api.getPlaylistTracks(userdata.id, playlist.id)
+          .then(function(data) {
+            // console.log(data);
+            playlist.songs = data;
+            callback2();
+          }, function(err) {
+            callback2(err);
+          });
+      }, function(err) {
+        console.log(err);
+        // callback(err);
+        callback();
       });
+    }
 
+  ], function(err, results) {
+    // if(err)
+    //   return next(err);
 
+    console.log('done');
+
+    res.render('index', {
+      title: 'Hello',
+      playlists: playlists
+    });
   });
 
 });
